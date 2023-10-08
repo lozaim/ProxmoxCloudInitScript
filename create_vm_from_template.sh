4207e2dfc6bf1944e52ce43a78af18fa720e26f0
lozaim <65390783+lozaim@users.noreply.github.com>
2023-10-08 17:34:23 +0300

    Create create_vm_from_template.sh

---
+++ create_vm_from_template.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+#
+# Scripfor for manually create VM from vm-template via CLI
+#
+echo ""
+echo " Proxmox Creation VM Tool"
+echo ""
+echo " Please follow the instructions"
+echo ""
+read -p " Please enter VM id: " vm_id
+
+echo $vm_id
+
+read -p " Please enter VM hostname: " vm_hostname
+
+echo $vm_hostname
+# Copy snippent to the new file, cnange hostname and apply snippet to template
+cp /var/lib/vz/snippets/4000.yaml /var/lib/vz/snippets/$vm_id-user-data.yaml
+sed -i "s/ubuntu/$vm_hostname/" /var/lib/vz/snippets/$vm_id-user-data.yaml
+qm set 4000 --cicustom "user=local:snippets/$vm_id-user-data.yaml"
+# Create VM from template
+pvesh create /nodes/prox2/qemu/4000/clone --newid $vm_id --full --name=$vm_hostname
+# Starting VM
+qm start $vm_id
+echo ""
+echo " VM Configuring. Please wait approx 120sec.. "
+echo ""
+# Timer
+for (( i=120; i>0; i--)); do
+  sleep 1 &
+  printf "  $i \r"
+  wait
+done
+# Check if VM still running
+while [ "$output"  != "status: stopped" ]; do
+  echo "VM still confuguring please wait"
+  sleep 5
+  output=$(eval "qm status $vm_id")
+done
+# When VM stopped, unlink cloudinit disk
+qm disk unlink $vm_id --idlist ide0 --force
+# Remove cloudinit string from VM config
+sed -i '/cicustom/d' /etc/pve/qemu-server/$vm_id.conf
+sed -i '/sshkeys/d' /etc/pve/qemu-server/$vm_id.conf
+# Apply standart snipped to vm-template
+qm set 4000 --cicustom "user=local:snippets/4000.yaml"
+# Starting VM
+qm start $vm_id
+# Check if VM still stopped
+while [ "$output"  != "status: running" ]; do
+  echo "VM starting. Please wait"
+  sleep 5
+  output=$(eval "qm status $vm_id")
+done
+
+echo ""
+echo " VM with id: $vm_id and with hostname: $vm_hostname had been created and configured"
+echo ""
