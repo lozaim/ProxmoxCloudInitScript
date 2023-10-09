65b0be45be26c38bc31cd75d283b310cbe5b62ab
lozaim <65390783+lozaim@users.noreply.github.com>
2023-10-09 19:50:19 +0300

    Update create-ubuntu-2204-template.sh

--- create-ubuntu-2204-template.sh
+++ create-ubuntu-2204-template.sh
@@ -5,7 +5,7 @@ imageName="ubuntu-22.04-server-cloudimg-amd64.img"
 diskName="ubuntu-22.04-server-cloudimg-amd64.qcow2"
 diskSize=20G
 volumeName="iso_ssd"
-ttemplateId="9000"
+templateId="9000"
 templateName="2204-templ"
 tmp_cores="2"
 tmp_memory="2048"
@@ -15,28 +15,28 @@ if [ ! -f "$imageName" ] ; then
     wget -O $imageName $imageURL
 fi
 # Destroy old template with same ID
-qm destroy $ttemplateId
+qm destroy $templateId
 echo ""
 echo " VM Configuring. Please wait... "
 echo ""
 # Create VM with parametrs
-qm create $ttemplateId --name $templateName --memory $tmp_memory --cores $tmp_cores --agent=1  --net0 virtio,bridge=vmbr1,tag=20
+qm create $templateId --name $templateName --memory $tmp_memory --cores $tmp_cores --agent=1  --net0 virtio,bridge=vmbr1,tag=20
 # Rename image to disk
 cp $imageName $diskName
 # Move and format disk
-qm importdisk $ttemplateId $imageName $volumeName --format qcow2
+qm importdisk $templateId $imageName $volumeName --format qcow2
 # Find  disk location and resize
-diskPath=$(eval "pvesm path $volumeName:$ttemplateId/vm-$ttemplateId-disk-0.qcow2")
+diskPath=$(eval "pvesm path $volumeName:$templateId/vm-$templateId-disk-0.qcow2")
 qemu-img resize $diskPath $diskSize
 # Attach device to VM
-qm set $ttemplateId --scsihw virtio-scsi-single 
-qm set $ttemplateId --scsi0 $volumeName:$ttemplateId/vm-$ttemplateId-disk-0.qcow2
-qm set $ttemplateId --boot c --bootdisk scsi0
-qm set $ttemplateId --ide0 $volumeName:cloudinit
-qm set $ttemplateId --serial0 socket --vga serial0
-qm set $ttemplateId --ipconfig0 ip=dhcp
+qm set $templateId --scsihw virtio-scsi-single 
+qm set $templateId --scsi0 $volumeName:$templateId/vm-$templateId-disk-0.qcow2
+qm set $templateId --boot c --bootdisk scsi0
+qm set $templateId --ide0 $volumeName:cloudinit
+qm set $templateId --serial0 socket --vga serial0
+qm set $templateId --ipconfig0 ip=dhcp
 # Create template from VM
-qm template $ttemplateId
+qm template $templateId
 echo ""
-echo " Template from VM with id: $ttemplateId and with name: $templateName had been created and configured"
+echo " Template from VM with id: $templateId and with name: $templateName had been created and configured"
 echo ""
